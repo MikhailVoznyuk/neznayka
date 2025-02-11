8b5daee4efa8019896b97522b91213968a6501ed
Mikhail Voznyuk <mikhailv.commerce@gmail.com>
2025-02-11 10:30:47 +0300

    v0.11

---
+++ app/[category]/[id]/page.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import DataBase from '@/lib/db/useDatabase'
+
+export default async function Page({params}) {
+    const db = new DataBase();
+    const articleId= (await params).id;
+    const article = await db.getArticleById(articleId);
+    return (
+        <main>
+            <div>
+                <h3>{article.title}</h3>
+                <h5>{article.description}</h5>
+                <p>{article.content}</p>
+
+            </div>
+        </main>
+    )
+}
---
+++ app/error/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function errorPage() {
+    return (
+        <div>
+            <h5>Страница находится в разработке</h5>
+            <Link href="/">Назад</Link>
+         </div>
+    )
+    
+}
